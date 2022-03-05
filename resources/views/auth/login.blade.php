b8617688fba589c631a5d2a36daa94c292870546
AbdullahAlMuhid <muhid@ihealthscreen.org>
2022-03-05 23:05:18 +0600

    added read me

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -53,8 +53,10 @@
 
                         <div class="row mb-0">
                             <div class="col-md-8 offset-md-4">
+
                                     {!! NoCaptcha::renderJs() !!}
                                     {!! NoCaptcha::display() !!}
+                                    
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
                                 </button>
